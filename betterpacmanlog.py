1f4d24544e6bec13bb2dbbb7ab22b7b03b7f19a0
Tony Dougal <96322007+tmdougal@users.noreply.github.com>
2026-05-01 13:46:09 -0400

    Update betterpacmanlog.py

--- betterpacmanlog.py
+++ betterpacmanlog.py
@@ -4,20 +4,26 @@
 
 filePath = "/var/log/pacman.log"
 
-p1Filter= "[PACMAN] Running"
+p1Filter1 = "[ALPM] installed "
+p1Filter2 = "[ALPM] transaction completed"
 
 allLinesList = []
 p1ParsedLinesList = []
 p1SkippedLinesList = []
 
+#Take each line from the log file and put into a list.
 with open (filePath, "rt") as logFile:
     for line in logFile:
         allLinesList.append(line.rstrip('\n'))
-        if p1Filter in line:
-            p1ParsedLinesList.append(line.rstrip('\n'))
-        else:
-            p1SkippedLinesList.append(line.rstrip('\n'))
+
+#For each index in the list, compare the current index to p1Filter1
+#and the next index to p1Filter2, and add to current to p1ParsedLinesList
+#if both criteria are met.
+for i in range(len(allLinesList) - 1):
+    if p1Filter1 in allLinesList[i]:
+        if p1Filter2 in allLinesList[i + 1]:
+            p1ParsedLinesList.append(allLinesList[i])
 
 for element in p1ParsedLinesList:
-    print(element)
+    print(element
 
